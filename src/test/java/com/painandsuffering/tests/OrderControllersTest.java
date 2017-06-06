c31204697bc8e4a606ecb2685582ea1f3f28f958
mrfloyd221 <mrfloyd221@gmail.com>
2017-06-07 00:14:41 +0300

    test package

---
+++ src/test/java/com/painandsuffering/tests/OrderControllersTest.java
@@ -0,0 +1,41 @@
+package com.painandsuffering.tests;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import com.painandsuffering.controller.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.junit.Before;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OrderControllersTest {
+    @Autowired
+    private OrderController controller;
+    @Autowired
+    private WebApplicationContext wac;
+    private MockMvc mockMvc;
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+    @Test
+    public void controllerLoaded() throws Exception{
+        assertThat(controller).isNotNull();
+    }
+    @Test
+    public void getAllOrders() throws Exception{
+
+    }
+    public void getOrder() throws Exception {
+        this.mockMvc.perform(get("/accounts/1").accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("$.name").value("Lee"));
+    }
+}
