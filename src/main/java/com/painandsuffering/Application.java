7122e134aef4a21874e1239bafc601200fccad84
mrfloyd221 <mrfloyd221@gmail.com>
2017-06-08 22:47:16 +0300

    Revert "nothing interesting"
    This reverts commit d7c3b03c2cd711edbfd35ec0f442db050b4bd3b0.

--- src/main/java/com/painandsuffering/Application.java
+++ src/main/java/com/painandsuffering/Application.java
@@ -13,7 +13,6 @@ import java.util.Arrays;
 @SpringBootApplication
 public class Application{
     public static void main(String[] args) throws Exception {
-
         SpringApplication.run(Application.class, args);
     }
 }
